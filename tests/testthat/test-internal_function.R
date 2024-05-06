0675049f22b558e22bbd0691e4d6a1dfad8772db
hwang <haiding.wang@pasteur.fr>
2024-05-06 11:39:40 +0200

    add  external.package.name

--- tests/testthat/test-internal_function.R
+++ tests/testthat/test-internal_function.R
@@ -1,12 +1,12 @@
 test_that(".pack_and_function_check works well", {
   # .pack_and_function_check should throw an error when fun argument doesn't contain '::'
-  expect_error(.pack_and_function_check(fun = "geom_point", lib.path = "/some/path", external.function.name = "fun1"))
+  expect_error(.pack_and_function_check(fun = "geom_point", lib.path = "/some/path", external.function.name = "fun1", external.package.name = "pack1"))
 
 
   # pack_and_function_check should throw an error when required package is not installed
-  expect_error(.pack_and_function_check(fun = "ggplot2::geom_point", lib.path = "/nonexistent/path", external.function.name = "fun1"))
+  expect_error(.pack_and_function_check(fun = "ggplot2::geom_point", lib.path = "/nonexistent/path", external.function.name = "fun1", external.package.name = "pack1"))
 
 
   # .pack_and_function_check should throw an error when required function is missing
-  expect_error(.pack_and_function_check(fun = "ggplot2::nonexistent_function", lib.path = "/some/path", external.function.name = "fun1"))
+  expect_error(.pack_and_function_check(fun = "ggplot2::nonexistent_function", lib.path = "/some/path", external.function.name = "fun1", external.package.name = "pack1"))
 })
